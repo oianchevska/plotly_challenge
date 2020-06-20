ccfe28148871106541ac504333d06bd97c9a2b4b
oianchevska <oianchevska@gmail.com>
2020-06-20 14:36:18 -0400

    HW-15: added final version

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,29 @@
 
 var barColors1 = ["#5899DA", "#E8743B", "#19A979", "#ED4A7B", "#945ECF", "#13A4B4", "#525DF4", "#BF399E", "#4dc3ff", "#EE6868"];
-//var col = ["#FF33F6","#3D0C3A","#320C3D","#290C3D","#1C0C3D","#370C3D","#CF24A3","#AE5497","#CE1B5A","#990439","#F5B041","#7D3C98","#138D75"]
-//var barColors = ["#E8F8F5","#D5F5E3","#82E0AA", "#48C9B0","#52BE80","#27AE60","#1E8449","#196F3D","#0B5345","#0E6251"];
-//var barColors = ["#ff794d","#ff794d","#ff6633", "#ff531a","#ff4000","#e63900","#cc3300","#b32d00","#992600","#802000"];
 var gaugeColors = ["#5899DA", "#E8743B", "#19A979", "#ED4A7B", "#945ECF", "#13A4B4", "#525DF4", "#BF399E", "#4dc3ff","rgba(255, 255, 255, 0)"]
+
+
 function build() {
 
-    console.log("build")
-
+    console.log("build");
     var dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a variable
     var dataset = dropdownMenu.property("value");
 
-    console.log(dataset);
-
+    // filtering subject id
     d3.json("static/js/samples.json").then(function (data) {
 
         var metadata=data.metadata.filter(function (rec) {
                 return rec.id==dataset;
         });
 
+        console.log(metadata)
+
         var samplesData=data.samples.filter(function (rec) {
             return rec.id==dataset;
         });
 
-        console.log(metadata);
+        console.log(samplesData)
+
         buidMetadata(metadata[0]);
         buidBar(samplesData[0]);
         buidСhart(samplesData[0]);
@@ -32,28 +31,16 @@ function build() {
 
     });
 
-    // 1--get id from dropdown
-    // 2--filter data by this id
-    // 3--fill all form
 };
 
 function buidBar(samplesData) {
-
-    //var barChart = d3.select("#bar");
-    // metaTable.selectAll("").remove();
-
-    console.log(samplesData);
 
     var id=samplesData.id;
     var otu_ids=samplesData.otu_ids;
     var otu_labels=samplesData.otu_labels;
     var sample_values=samplesData.sample_values;
-    console.log(otu_ids);
 
     var otuStr=otu_ids.map(a => 'OTU '.concat(a));
-    // var sample_values_sorted=sample_values.sort(function (a,b)  {
-    //     return b-a
-    //     });
 
     var trace= { x: sample_values.slice(0,10).reverse(),
                  y: otuStr.slice(0,10).reverse(),
@@ -80,16 +67,12 @@ function buidBar(samplesData) {
 
 function buidСhart(samplesData) {
 
-    //var bubbleChart = d3.select("#bubble");
-    // metaTable.selectAll("").remove();
-
-    console.log(samplesData);
 
     var id=samplesData.id;
     var otu_ids=samplesData.otu_ids;
     var otu_labels=samplesData.otu_labels;
     var sample_values=samplesData.sample_values;
-    console.log(otu_ids);
+
 
     var trace1 = {
         x: otu_ids,
@@ -110,8 +93,8 @@ function buidСhart(samplesData) {
     var layout = {
         title: '<b>Belly Button Bubble Chart</b>',
         showlegend: false,
-        height: 800,
-        width: 1400
+        height: 600,
+        width: 1200
     };
 
     Plotly.newPlot("bubble", data,layout)
@@ -121,14 +104,14 @@ function buidСhart(samplesData) {
 
 function buidMetadata(metadata) {
 
-    // console.log("Metadata");
 
     var metaTable = d3.select("#sample-metadata");
-    metaTable.selectAll("h6").remove();
+    metaTable.selectAll("tr").remove();
 
     Object.entries(metadata).forEach(function ([key,value]) {
-
-        metaTable.append("h6").text(key+":"+value);
+       tr = metaTable.append("tr");
+       tr.append("td").text(key);
+       tr.append("td").text(value);
     });
 
 
@@ -137,14 +120,12 @@ function buidMetadata(metadata) {
 
 function buidGauge(metadata) {
 
-    console.log("Gauge")
 
     if (metadata.wfreq==null) {
         metadata.wfreq=0
     };
-        // Enter the washing frequency between 0 and 180
-        var level = parseFloat(metadata.wfreq) * 20;
 
+        var level = parseFloat(metadata.wfreq) * 20;
 
         var degrees = 180 - level;
         var radius = 0.5;
@@ -179,18 +160,6 @@ function buidGauge(metadata) {
                 textposition: "inside",
                 marker: {
                      colors: gaugeColors
-                    //     "rgba(0, 105, 11, .5)",
-                    //     "rgba(10, 120, 22, .5)",
-                    //     "rgba(14, 127, 0, .5)",
-                    //     "rgba(110, 154, 22, .5)",
-                    //     "rgba(170, 202, 42, .5)",
-                    //     "rgba(202, 209, 95, .5)",
-                    //     "rgba(210, 206, 145, .5)",
-                    //     "rgba(232, 226, 202, .5)",
-                    //     "rgba(240, 230, 215, .5)",
-                    //     "rgba(255, 255, 255, 0)"
-                    // ]
-
                 },
                 labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
                 hoverinfo: "label",
@@ -212,8 +181,8 @@ function buidGauge(metadata) {
                 }
             ],
             title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
-            height: 500,
-            width: 500,
+            height: 550,
+            width: 420,
             xaxis: {
                 zeroline: false,
                 showticklabels: false,
@@ -236,14 +205,11 @@ function buidGauge(metadata) {
 function init () {
 
     console.log("init");
-
     d3.json("static/js/samples.json").then(function (data) {
-
-
         var names = data.names;
-
         var dropdownMenu = d3.select("#selDataset");
 
+        console.log(names);
         names.forEach(function (name) {
             dropdownMenu.append("option").property("value", name).text(name);
         });
@@ -254,50 +220,12 @@ function init () {
 };
 
 
-        //  var id=samples.id;
-        //  var otu_ids=samples.otu_ids;
-        //  var otu_labels=samples.otu_labels;
-        //  var sample_values=samples.sample_values;
-        //
-        //  var otuStr=otu_ids.map(a => 'OTU '.concat(a));
-        //  console.log(otuStr);
-
-         // var sample_values_sorted=sample_values.sort(function (a,b)  {
-         //     return b-a
-         // })
-
- //         var trace= { x: sample_values.slice(0,10).reverse(),
- //             y: otuStr.slice(0,10).reverse(),
- //             text:otu_labels,
- //             marker: {
- //                 color: 'rgb(158,202,225)',
- //                 line: {
- //                     color: barColors,
- //                     width: 5
- //                 }},
- //             type: "bar",
- //             orientation: "h"
- //         };
- //
- //         var layout = {
- //             title: '<b>Belly Button Biodiversity</b>',
- //         };
- //
- //         var data = [trace]
- //
- //         Plotly.newPlot("plot", data,layout);
- //
- //
- //     });
- // };
-
-
-// On change to the DOM, call getData()
-
+//event action
 d3.selectAll("#selDataset").on("change", dataSubmit);
 
+//update
 function dataSubmit() {
-    console.log("dataSubmit")
+    console.log("update");
     build();
 };
 
